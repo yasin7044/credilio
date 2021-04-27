0d22ba9e7f70f804d589f2d9c3bbe28b316d3fa0
yasin_codelogicx2020 <yasin@codelogicx.com>
2021-04-27 22:20:52 +0530

    Fix Designing And Added Alert Box on Creating And Removing task

---
+++ src/components/SuccessComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :class="[
+      type === 'success'
+        ? 'bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative'
+        : 'bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative',
+,
+    ]"
+  >
+    <strong class="font-bold">{{ title }} </strong>
+    <p class="block sm:inline">{{ successMessage }}.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: "true", default: "Success" },
+    successMessage: { type: String, required: "true", default: "Success" },
+    type: { type: String, required: "true", default: "Success" },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/TaskComponent.vue
+++ src/components/TaskComponent.vue
@@ -1,103 +1,121 @@
 <template>
-  <div class="h-100 w-full flex justify-center font-sans">
-    
-    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-      <div class="mb-4">
-        <h1 class="font-bold">#todo</h1>
-        <div class="flex mt-4 justify-between">
-          <div
-            @click="selectedActiveTab('all')"
-            :class="[
-              activeClass === 'all' ? 'underline' : '',
-              activeClass === 'all' ? 'text-blue-500' : '',
-            ]"
-          >
-            All
-          </div>
-          <div
-            @click="selectedActiveTab('Active')"
-            :class="[
-              activeClass === 'Active' ? 'underline' : '',
-              activeClass === 'Active' ? 'text-blue-500' : '',
-            ]"
-          >
-            Active
-          </div>
-          <div
-            @click="selectedActiveTab('Completed')"
-            :class="[
-              activeClass === 'Completed' ? 'underline' : '',
-              activeClass === 'Completed' ? 'text-blue-500' : '',
-            ]"
-          >
-            Completed
-          </div>
-        </div>
-        <div class="flex mt-4">
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-            placeholder="Add Todo"
-            v-model="newTask"
-          />
-          <button
-            class="flex-no-shrink p-2 w-20 bg-blue-600 text-white rounded"
-            @click.enter="addNewTask()"
-          >
-            Add
-          </button>
-        </div>
-      </div>
-      <div>
-        <!--  {{taskArray}}
-          {{taskActive}} -->
-        <div
-          class="flex mb-4 justify-between"
-          v-for="(eachTask, index) in taskActive"
-          :key="index"
-        >
-          <div class="">
-            <!-- {{eachTask.isCompleted}} -->
-            <input
-              type="checkbox"
-              v-model="eachTask.isCompleted"
-              class="checked:bg-blue-600 checked:border-transparent mr-2"
-            />
-            <span
-              class="text-black-700"
-              :class="{
-                'line-through': eachTask.isCompleted,
-              }"
+  <div>
+    <div class="w-full flex justify-center font-sans">
+      <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        <div class="mb-4">
+          <h1 class="font-bold">#todo</h1>
+          <div class="flex mt-4 justify-between">
+            <div
+              @click="selectedActiveTab('all')"
+              :class="[
+                activeClass === 'all' ? 'underline' : '',
+                activeClass === 'all' ? 'text-blue-500' : '',
+              ]"
             >
-              {{ eachTask.description }}
-            </span>
+              All
+            </div>
+            <div
+              @click="selectedActiveTab('Active')"
+              :class="[
+                activeClass === 'Active' ? 'underline' : '',
+                activeClass === 'Active' ? 'text-blue-500' : '',
+              ]"
+            >
+              Active
+            </div>
+            <div
+              @click="selectedActiveTab('Completed')"
+              :class="[
+                activeClass === 'Completed' ? 'underline' : '',
+                activeClass === 'Completed' ? 'text-blue-500' : '',
+              ]"
+            >
+              Completed
+            </div>
+          </div>
+          <div class="flex mt-4">
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+              placeholder="Add Todo"
+              v-model="newTask"
+              @keyup.enter="addNewTask()"
+            />
+            <button
+              class="flex-no-shrink p-2 w-20 bg-blue-600 text-white rounded"
+              @click.enter="addNewTask()"
+            >
+              Add
+            </button>
+          </div>
+        </div>
+        <div>
+          <div
+            class="flex mb-4 justify-between items-center flex-1 flex-wrap"
+            v-for="(eachTask, index) in taskActive"
+            :key="index"
+          >
+            <div style="flex: 3" class="flex items-baseline flex-shrink">
+              <input
+                v-model="eachTask.isCompleted"
+                class="checked:bg-blue-600 checked:border-transparent mr-2"
+                type="checkbox"
+              />
+              <p
+                class="text-black-700 text-left break-all leading-4"
+                :class="{
+                  'line-through': eachTask.isCompleted,
+                }"
+              >
+                {{ eachTask.description }}
+              </p>
+            </div>
+            <button
+              class="flex-no-shrink p-1 ml-2 border-2"
+              style="flex: 0"
+              v-if="activeClass === 'Completed'"
+              @click="removingTask(eachTask.id)"
+            >
+              Remove
+            </button>
           </div>
           <button
-            v-if="activeClass === 'Completed'"
-            class="flex-no-shrink p-1 ml-2 border-2"
-            @click="removingTask(eachTask.id)"
+            v-if="activeClass === 'Completed' && taskActive.length"
+            class="flex-no-shrink p-1 ml-2 border-2 float-right"
+            @click="removeAllTask()"
           >
-            Remove
+            Remove All
           </button>
         </div>
-        <button
-          v-if="activeClass === 'Completed' && taskActive.length"
-          class="flex-no-shrink p-1 ml-2 border-2 float-right"
-          @click="removeAllTask()"
-        >
-          Remove All
-        </button>
       </div>
+    </div>
+    <div
+      class="absolute bottom-0 right-0 h-16 w-auto px-4 my-20"
+      v-if="showAlert"
+    >
+      <success-component
+        :title="title"
+        :successMessage="successMessage"
+        :type="type"
+      ></success-component>
     </div>
   </div>
 </template>
 
 <script>
+import SuccessComponent from "./SuccessComponent.vue";
 export default {
+  components: {
+    SuccessComponent,
+  },
   data() {
     return {
       newTask: "",
       activeClass: "all",
       taskArray: [],
+      showAlert: false,
+      title: "",
+      successMessage: "",
+      type: "",
     };
   },
   computed: {
@@ -118,17 +136,37 @@ export default {
       this.activeClass = tab;
     },
     addNewTask() {
-      this.taskArray.push({
-        id: new Date().getTime(),
-        description: this.newTask,
-        isCompleted: false,
-      });
-      this.newTask = "";
-      console.log(this.taskArray);
+      if (!this.newTask) {
+        this.showAlertMessage(
+          "Error!!",
+          "Please enter task notes",
+          "error",
+          "3000"
+        );
+      } else {
+        this.taskArray.push({
+          id: new Date().getTime(),
+          description: this.newTask,
+          isCompleted: false,
+        });
+        this.newTask = "";
+        this.showAlertMessage(
+          "Created!!",
+          "New Task Added Successfully",
+          "success",
+          "3000"
+        );
+      }
     },
     removingTask(id) {
       const index = this.taskArray.findIndex((ele) => ele.id === id);
       this.taskArray.splice(index, 1);
+      this.showAlertMessage(
+        "Remove!!",
+        "Single Item remove Successfully",
+        "danger",
+        "3000"
+      );
     },
     removeAllTask() {
       const spliceArray = this.taskArray
@@ -137,6 +175,21 @@ export default {
       this.taskArray = this.taskArray.filter(
         (ele) => !spliceArray.includes(ele.id)
       );
+      this.showAlertMessage(
+        "Remove!!",
+        "All Complete Item Remove Successfully",
+        "danger",
+        "3000"
+      );
+    },
+    showAlertMessage(title, successMessage, type, time = 3000) {
+      this.showAlert = true;
+      this.title = title;
+      this.successMessage = successMessage;
+      this.type = type;
+      setTimeout(() => {
+        this.showAlert = false;
+      }, time);
     },
   },
 };
